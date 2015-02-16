2a6f235b8425ebe2b8f8549527f4da471e0e85ea
Gil Forcada <gforcada@gnome.org>
2015-02-16 10:16:11 +0100

    Specific feitag.de changes
    der.freitag is using its own workflow for comments,
    the logic to know if a comment is visible or not is based on a
    workflow status rather than a permission.

--- plone/app/discussion/conversation.py
+++ plone/app/discussion/conversation.py
@@ -73,8 +73,13 @@ class Conversation(Traversable, Persistent, Explicit):
         return parent.restrictedTraverse("@@conversation_view").enabled()
 
     def total_comments(self):
+        wft = getToolByName(self, 'portal_workflow')
+        workflow_id = 'freitag_comment_workflow'
+
         public_comments = [
             x for x in self.values() if user_nobody.has_permission("View", x)
+            and wft.getStatusOf(workflow_id, x)
+            and wft.getStatusOf(workflow_id, x)['review_state'] == 'visible'
         ]
         return len(public_comments)
 
