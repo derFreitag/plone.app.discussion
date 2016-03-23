437dfcb2d09f7479eedec119c1098c4eecc17933
Gil Forcada <gforcada@gnome.org>
2016-03-23 10:39:09 +0100

    index/reindex/unindex the object itself
    Do not defer to p.a.discussion own tool.
    
    That's needed so that collective.indexing can patch comments,
    so in turn can be indexed by Solr.

--- plone/app/discussion/subscribers.py
+++ plone/app/discussion/subscribers.py
@@ -5,19 +5,16 @@ from Products.CMFCore.utils import getToolByName
 def index_object(obj, event):
     """Index the object when it is added to the conversation.
     """
-    catalog = getToolByName(obj, 'portal_catalog')
-    return catalog.reindexObject(obj)
+    obj.indexObject()
 
 
 def unindex_object(obj, event):
     """Unindex the object when it is removed from the conversation.
     """
-    catalog = getToolByName(obj, 'portal_catalog')
-    return catalog.unindexObject(obj)
+    obj.unindexObject()
 
 
 def modified_object(obj, event):
     """Reindex the object when it is modified.
     """
-    catalog = getToolByName(obj, 'portal_catalog')
-    return catalog.reindexObject(obj)
+    obj.reindexObject()
